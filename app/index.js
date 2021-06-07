addb2a4c5ae4144a6fd09be351a95f10e1d2002c
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-06-07 21:05:06 +0100

    Finishing styling plus input validation

--- app/index.js
+++ app/index.js
@@ -13,33 +13,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/output", (req, res) => {
-  res.render('output');
-});
-
-app.get("/test", (req, res) => {
-  res.render('test');
-});
-
-app.get("/dice", (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST" action="./dice">
-        <input name="dice" placeholder="enter the number of dice to roll" />
-        <input name="sides" placeholder="how many sides have your dice?" />
-        
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post("/dice", (req, res) => {
+app.post("/output", (req, res) => {
   const { dice, sides } = req.body;
+
   const results = [];
   for (let i = 0; i < parseInt(dice); i++) {
     results.push(Math.floor(Math.random() * parseInt(sides)) + 1);
@@ -48,8 +24,13 @@ app.post("/dice", (req, res) => {
     return total + amount;
   }, 0);
 
-  res.send(`dice rolled! results: ${results}. Total ${total}`);
+  res.render('output', { results: results, total: total });
 });
+
+app.get("/test", (req, res) => {
+  res.render('home');
+});
+
 
 app.listen(3001, () => {
   console.log("listening on port 3001!");
