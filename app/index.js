b7afefd55aff5e52334ea5cbeef00b6413885385
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-06-08 21:28:13 +0100

    Able to render template with errors
    toy error middleware

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
+
 const path = require('path');
 
 const app = express();
@@ -12,22 +14,48 @@ app.set('view engine', 'ejs');
 // eslint-disable-next-line no-undef
 app.set('views', path.join(__dirname, '/views'));
 
-
-app.post("/output", (req, res) => {
-  const { dice, sides } = req.body;
-
-  const results = [];
-  for (let i = 0; i < parseInt(dice); i++) {
-    results.push(Math.floor(Math.random() * parseInt(sides)) + 1);
-  }
-  const total = results.reduce((total, amount) => {
-    return total + amount;
-  }, 0);
-
-  res.render('output', { results: results, total: total });
+app.use(bodyParser.json());
+app.use(function (err, req, res, next) {
+  // logic
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
-app.get("/test", (req, res) => {
+
+app.post("/output",
+  [
+    body('dice')
+      .isInt()
+      .withMessage("Please enter an Integer.")
+      .matches("[1-6]")
+      .withMessage("Pass an integer as number of dice. Max 6 - Min 1"),
+    body("sides")
+      .isInt()
+      .withMessage("Sides number not valid")
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const alert = errors.array();
+      res.render('home', { alert: alert });
+
+      // return res.status(400).send(errors.array());
+
+    }
+    const { dice, sides } = req.body;
+
+    const results = [];
+    for (let i = 0; i < parseInt(dice); i++) {
+      results.push(Math.floor(Math.random() * parseInt(sides)) + 1);
+    }
+    const total = results.reduce((total, amount) => {
+      return total + amount;
+    }, 0);
+
+    res.render('output', { results: results, total: total });
+  });
+
+app.get("/", (req, res) => {
   res.render('home');
 });
 
